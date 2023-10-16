9efb5389489871b7b851ea323ef18753c2630f76
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2023-10-16 21:53:04 +0200

    Add JSDOC for the constructor and fix lint issues

--- src/Player.js
+++ src/Player.js
@@ -11,29 +11,37 @@ import { PlayingCard } from './PlayingCard'
  * Represents the player.
  */
 export class Player {
-/**
- * The players cards on hand.
- *
- * @type {PlayingCard[]}
- */
-#hand
+  /**
+   * The players cards on hand.
+   *
+   * @type {PlayingCard[]}
+   */
+  #hand
 
-/**
- * The players nickname.
- *
- * @type {string}
- */
-#nickname
+  /**
+   * The players nickname.
+   *
+   * @type {string}
+   */
+  #nickname
 
-/**
- * The value sum of the cards on the players hand.
- *
- * @type {number}
- */
-#standValue
+  /**
+   * The value sum of the cards on the players hand.
+   *
+   * @type {number}
+   */
+  #standValue
 
-constructor (nickname: String, standValue: number = 14) {
+  /**
+   * Initializes a new instance of the player class.
+   *
+   * @param {string} nickname - The nickname of the player.
+   * @param {number} standValue - The sum of the cards on the players hand.
+   */
+  constructor (nickname, standValue = 14) {
     this.#nickname = nickname
     this.#standValue = standValue
-}
+
+    this.#hand = []
+  }
 }
