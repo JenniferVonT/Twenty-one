c159f9b79f0706339141a47b12d64102bc2921bc
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2023-10-23 20:46:02 +0200

    Add all the private fields in the class CardTable module

--- src/CardTable.js
+++ src/CardTable.js
@@ -13,5 +13,31 @@ import { Deck } from './Deck.js'
  * Represents a Card Table.
  */
 export class CardTable {
-    
+  /**
+   *Refers to a player object that represents the dealer.
+   *
+   * @type {Player}
+   */
+  #dealer
+
+  /**
+   * Refers to the card deck.
+   *
+   * @type {Deck}
+   */
+  #deck
+
+  /**
+   * Refers to the throw pile/deck.
+   *
+   * @type {PlayingCard[]}
+   */
+  #discardPile
+
+  /**
+   * Refers to all the players in the game.
+   *
+   * @type {Player[]}
+   */
+  #players
 }
